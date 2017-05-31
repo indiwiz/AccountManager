b166ef4cebbebeb55cd3969afac155e27a068ba6
Deven Shah <deven.shah@capita.co.uk>
2017-05-31 10:54:28 +0100

    Configured standard response on exception

--- AccountManager.Api/Startup.cs
+++ AccountManager.Api/Startup.cs
@@ -8,6 +8,7 @@ using AccountManager.DataAccess.Entities;
 using AccountManager.DataAccess.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -50,7 +51,14 @@ namespace AccountManager.Api
             }
             else
             {
-                app.UseExceptionHandler();
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault occurred. Please try again later.");
+                    });
+                });
             }
 
             AutoMapper.Mapper.Initialize(cfg =>
